99233db5c17b30debc37e3b9f3134cee7b3f8052
Guzhuo <1749026382@qq.com>
2019-03-13 23:44:44 +0800

    03/13 23:43 Chapter6 FilePersistenceTest

---
+++ Chapter6/FilePersistenceTest/app/src/main/java/com/guzhuo/filepersistencetest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.guzhuo.filepersistencetest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.editText);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = editText.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
